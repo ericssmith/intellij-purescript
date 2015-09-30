86f28850be012a9fab7b66b9fea3a8b4cb1d474f
ericssmith <eric2.71828@gmail.com>
2015-09-30 13:02:32 -0400

    Add files for "9. Completion Contributor"

---
+++ src/com/demo/PurescriptCompletionContributor.java
@@ -0,0 +1,23 @@
+package com.demo;
+
+import  com.intellij.codeInsight.completion.* ;
+import  com.intellij.codeInsight.lookup.LookupElementBuilder ;
+import  com.intellij.patterns.PlatformPatterns ;
+import  com.intellij.util.ProcessingContext ;
+import  com.demo.psi.PurescriptTypes ;
+import  org.jetbrains.annotations.NotNull ;
+
+public class PurescriptCompletionContributor extends CompletionContributor {
+    public PurescriptCompletionContributor() {
+        extend(CompletionType.BASIC,
+                PlatformPatterns.psiElement(PurescriptTypes.VALUE).withLanguage(PurescriptLanguage.INSTANCE),
+                new CompletionProvider<CompletionParameters>() {
+                    public void addCompletions(@NotNull CompletionParameters parameters,
+                                               ProcessingContext context,
+                                               @NotNull CompletionResultSet resultSet) {
+                        resultSet.addElement(LookupElementBuilder.create("Hello"));
+                    }
+                }
+        );
+    }
+}
