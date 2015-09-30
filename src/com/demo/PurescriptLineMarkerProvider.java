185b19077f8350b2b585fb10129ffc6fe35c3a18
ericssmith <eric2.71828@gmail.com>
2015-09-30 12:56:01 -0400

    Add files for "8. Line Marker". The language specified in registration in plugin.xml is Java. Not sure how useful this will be and may need to be removed. An icon is inserted into the Java file, and you can navigate by clicking on it, but this does not provide "Go To Declaration".

---
+++ src/com/demo/PurescriptLineMarkerProvider.java
@@ -0,0 +1,35 @@
+package com.demo;
+
+
+import com.intellij.codeInsight.daemon.RelatedItemLineMarkerInfo;
+import com.intellij.codeInsight.daemon.RelatedItemLineMarkerProvider;
+import com.intellij.codeInsight.navigation.NavigationGutterIconBuilder;
+import com.intellij.openapi.project.Project;
+import com.intellij.psi.PsiElement;
+import com.intellij.psi.PsiLiteralExpression;
+import com.demo.psi.PurescriptProperty;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.Collection;
+import java.util.List;
+
+public class PurescriptLineMarkerProvider extends RelatedItemLineMarkerProvider {
+    @Override
+    protected void collectNavigationMarkers(@NotNull PsiElement element, Collection<? super RelatedItemLineMarkerInfo> result) {
+        if (element instanceof PsiLiteralExpression) {
+            PsiLiteralExpression literalExpression = (PsiLiteralExpression) element;
+            String value = (String) literalExpression.getValue();
+            if (value != null && value.startsWith("simple:")) {
+                Project project = element.getProject();
+                final List<PurescriptProperty> properties = PurescriptUtil.findProperties(project, value.substring(7));
+                if (properties.size() > 0) {
+                    NavigationGutterIconBuilder<PsiElement> builder =
+                            NavigationGutterIconBuilder.create(PurescriptIcons.FILE).
+                                    setTargets(properties).
+                                    setTooltipText("Navigate to a simple property");
+                    result.add(builder.createLineMarkerInfo(element));
+                }
+            }
+        }
+    }
+}
